94ef545435e0e88a28ea53b9eab7cf69e8ebce66
Zeki Ahmet Bayar <zekiahmetbayar1@gmail.com>
2020-12-08 00:14:18 +0300

    Veri tabanından veri çekme işlemleri tamamlandı

--- deneme.py
+++ deneme.py
@@ -1,62 +1,29 @@
+#!/usr/bin/env python
+
+import gi
+gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
-from gi.repository import Pango
-import sys
 
-columns = ["First Name",
-           "Last Name",
-           "Phone Number"]
+window = Gtk.Window()
+window.set_default_size(200, 200)
+window.connect("destroy", Gtk.main_quit)
 
-phonebook = [["Jurg", "Billeter", "555-0123"],
-             ["Johannes", "Schmid", "555-1234"],
-             ["Julita", "Inca", "555-2345"],
-             ["Javier", "Jardon", "555-3456"],
-             ["Jason", "Clinton", "555-4567"],
-             ["Random J.", "Hacker", "555-5678"]]
+overlay = Gtk.Overlay()
+window.add(overlay)
 
+textview = Gtk.TextView()
+textview.set_wrap_mode(Gtk.WrapMode.WORD_CHAR)
+textbuffer = textview.get_buffer()
+textbuffer.set_text("Welcome to the PyGObject Tutorial\n\nThis guide aims to provide an introduction to using Python and GTK+.\n\nIt includes many sample code files and exercises for building your knowledge of the language.", -1)
+overlay.add(textview)
 
-class MyWindow(Gtk.ApplicationWindow):
+button = Gtk.Button(label="Overlayed Button")
+button.set_valign(Gtk.Align.CENTER)
+button.set_halign(Gtk.Align.CENTER)
+overlay.add_overlay(button)
 
-    def __init__(self, app):
-        Gtk.Window.__init__(self, title="My Phone Book", application=app)
-        self.set_default_size(250, 100)
-        self.set_border_width(10)
+overlay.show_all()
 
-        listmodel = Gtk.ListStore(str, str, str)
-        for i in range(len(phonebook)):
-            listmodel.append(phonebook[i])
+window.show_all()
 
-
-        view = Gtk.TreeView(model=listmodel)
-        for i, column in enumerate(columns):
-            cell = Gtk.CellRendererText()
-
-            if i == 0:
-                cell.props.weight_set = True
-                cell.props.weight = Pango.Weight.BOLD
-
-            col = Gtk.TreeViewColumn(column, cell, text=i)
-            view.append_column(col)
-
-
-        grid = Gtk.Grid()
-        grid.attach(view, 0, 0, 1, 1)
-
-
-        self.add(grid)
-
-
-class MyApplication(Gtk.Application):
-
-    def __init__(self):
-        Gtk.Application.__init__(self)
-
-    def do_activate(self):
-        win = MyWindow(self)
-        win.show_all()
-
-    def do_startup(self):
-        Gtk.Application.do_startup(self)
-
-app = MyApplication()
-exit_status = app.run(sys.argv)
-sys.exit(exit_status)
+Gtk.main()
--- main.py
+++ main.py
@@ -6,17 +6,8 @@ from gi.repository import Gtk
 gi.require_version('Gdk', '3.0')
 from gi.repository import Gdk, GLib,Pango
 
-
-columns = ["First Name",
-           "Last Name",
-           "Phone Number"]
-
-phonebook = [["Jurg", "Billeter", "555-0123"],
-             ["Johannes", "Schmid", "555-1234"],
-             ["Julita", "Inca", "555-2345"],
-             ["Javier", "Jardon", "555-3456"],
-             ["Jason", "Clinton", "555-4567"],
-             ["Random J.", "Hacker", "555-5678"]]
+hasta_columns = ["ID", "TC NO", "First Name", "Last Name", "EMAIL"]
+ilac_columns = ["ID", "NAME", "DOSE", "ACTIVE", "PIECE", "PRICE"]
 
 class MyWindow(Gtk.Window):
 
@@ -29,10 +20,8 @@ class MyWindow(Gtk.Window):
         self.main()
 
     def main(self):
-
         self.baglanti_baslat()
-        self.kullanici_tablo_olustur()
-        self.ana_ekran()
+        self.giris_ekrani()
 
     #### Ekranlar #####
 
@@ -88,51 +77,13 @@ class MyWindow(Gtk.Window):
 
         self.add(self.kayit_Table)
         self.show_all()
-
-    satis_Table = Gtk.Table(n_rows=10, n_columns=10, homogeneous=True)
-    
-    def satis_ekrani(self):
-        
-        self.tablo_gosterimi()
-
-        satis_patienceLabel = Gtk.Label(label = "Patients")
-        satis_patientSearch = Gtk.SearchEntry()
-        #satis_searchEntry.connect("activate")
-        satis_patienceAddButton = Gtk.Button(label = "Add")
-        
-        satis_cartLabel = Gtk.Label(label = "Cart")
-        satis_cartCleanButton = Gtk.Button(label = "Clean")
-
-        satis_medicineSearch = Gtk.SearchEntry()
-        satis_medicineLabel = Gtk.Label(label = "Medicines")
-
-        #self.add(self.satis_Table)
-        self.satis_Table.attach(satis_patienceLabel,0,3,0,1)
-        self.satis_Table.attach(satis_patientSearch,0,2,1,2)
-        self.satis_Table.attach(satis_patienceAddButton,2,3,1,2)
-        self.satis_Table.attach(self.view,0,3,2,5)
-
-        self.tablo_gosterimi()
-
-        self.satis_Table.attach(satis_cartLabel,0,2,6,7)
-        self.satis_Table.attach(satis_cartCleanButton,2,3,6,7)
-        self.satis_Table.attach(self.view,0,3,7,10)
-    
-        
-
-        self.satis_Table.attach(satis_medicineLabel,3,10,0,1)
-        self.satis_Table.attach(satis_medicineSearch,3,10,1,2)
-        self.tablo_gosterimi()
-        self.satis_Table.attach(self.view,3,10,2,10)
-
-        self.satis_Table.show_all()
-
     
     def ana_ekran(self):
         self.notebook = Gtk.Notebook()
         self.add(self.notebook)
-        self.satis_ekrani()
         
+        self.satis_ekrani()
+        self.alis_ekrani()
         
         self.page1 = Gtk.Box()
         self.page1.set_border_width(10)
@@ -140,9 +91,10 @@ class MyWindow(Gtk.Window):
         self.page1.add(self.satis_Table)
         self.notebook.append_page(self.page1, Gtk.Label(label="Sell"))
 
-
         self.page2 = Gtk.Box()
         self.page2.set_border_width(10)
+        self.page2.set_homogeneous(True)
+        self.page2.add(self.alis_Table)
         self.page2.add(self.satis_Table)
         self.notebook.append_page(self.page2, Gtk.Label(label="Buy"))
 
@@ -159,26 +111,137 @@ class MyWindow(Gtk.Window):
         self.page4 = Gtk.Box()
         self.page4.set_border_width(10)
         self.page4.add(Gtk.Label(label="Default Page!"))
-        self.notebook.append_page(self.page4, Gtk.Label(label="Patients"))       
-        
-
+        self.notebook.append_page(self.page4, Gtk.Label(label="Settings"))       
+    
         self.notebook.show_all()
 
-    def tablo_gosterimi(self):
-        listmodel = Gtk.ListStore(str, str, str)
-        for i in range(len(phonebook)):
-            listmodel.append(phonebook[i])
+    def satis_ekrani(self):
+        
+        self.hasta_tablo()
+        self.satis_Table = Gtk.Table(n_rows=10, n_columns=10, homogeneous=False)
+
+        satis_patienceLabel = Gtk.Label(label = "Patients")
+        satis_patientSearch = Gtk.SearchEntry()
+        #satis_searchEntry.connect("activate")
+        satis_patienceAddButton = Gtk.Button(label = "Add")
+        
+        self.cart_tablo()
+        satis_cartLabel = Gtk.Label(label = "Cart")
+        satis_cartCleanButton = Gtk.Button(label = "Clean")
+
+        self.ilac_tablo()
+        satis_medicineSearch = Gtk.SearchEntry()
+        satis_medicineLabel = Gtk.Label(label = "Medicines")
+
+        self.satis_Table.attach(satis_patienceLabel,0,3,0,1)
+        self.satis_Table.attach(satis_patientSearch,0,2.5,1,2)
+        self.satis_Table.attach(satis_patienceAddButton,2,3,1,2)
+        self.satis_Table.attach(self.view,0,3,2,5)
+
+        self.satis_Table.attach(satis_cartLabel,0,2,6,7)
+        self.satis_Table.attach(satis_cartCleanButton,2,3,6,7)
+        self.satis_Table.attach(self.cart_view,0,3,7,10)
+    
+        self.satis_Table.attach(satis_medicineLabel,3,10,0,1)
+        self.satis_Table.attach(satis_medicineSearch,3,10,1,2)
+
+        self.satis_Table.attach(self.ilac_view,3,10,2,10)
+
+        self.satis_Table.show_all()
+    
+    def alis_ekrani(self):
+
+        self.alis_Table = Gtk.Table(n_rows=10, n_columns=10, homogeneous=False)
+
+        alis_factoriesLabel = Gtk.Label(label = "Factories")
+        alis_searchEntry = Gtk.SearchEntry()
+        #satis_searchEntry.connect("activate")
+        
+        alis_cartLabel = Gtk.Label(label = "Cart")
+        alis_cartCleanButton = Gtk.Button(label = "Clean")
+
+        alis_medicineSearch = Gtk.SearchEntry()
+        alis_medicineLabel = Gtk.Label(label = "Medicines")
+
+        self.alis_Table.attach(alis_factoriesLabel,0,3,0,1)
+        self.alis_Table.attach(alis_searchEntry,0,3,1,2)
+        self.alis_Table.attach(self.view,0,3,2,5)
+
+        self.alis_Table.attach(alis_cartLabel,0,2,6,7)
+        self.alis_Table.attach(alis_cartCleanButton,2,3,6,7)
+        self.alis_Table.attach(self.view,0,3,7,10)
+    
+        self.alis_Table.attach(alis_medicineLabel,3,10,0,1)
+        self.alis_Table.attach(alis_medicineSearch,3,10,1,2)
+
+        self.alis_Table.attach(self.view,3,10,2,10)
+
+        self.alis_Table.show_all()
+
+    ### Yan Ekranlar ###
+
+    def hasta_ekle(self):
+        self.add_PatientWindow = Gtk.Window()
+        self.input_window.set_title("Add New Patient")
+        self.input_window.set_border_width(10)
+        table2 = Gtk.Table(n_rows=7, n_columns=0, homogeneous=True)
+        self.input_window.add(table2)
+
+        self.host = Gtk.Entry()
+        self.host_name = Gtk.Entry()
+        self.user = Gtk.Entry()
+        self.port = Gtk.Entry()
+        self.submit_button = Gtk.Button(label ="Gönder")
+        self.submit_button.connect('clicked',self.on_click_add_new_host)
+  
+        self.host.set_placeholder_text("Host")
+        self.host_name.set_placeholder_text("HostName")
+        self.user.set_placeholder_text("User")
+
+        table2.attach(self.host,0,1,0,1)
+        table2.attach(self.host_name,0,1,2,3)
+        table2.attach(self.user,0,1,4,5)
+        table2.attach(self.submit_button,0,1,6,7)
+
+        self.input_window.present()
+        self.input_window.show_all()
+
+    ### Tablolar ###
+
+    def hasta_tablo(self):
+        self.hasta_vericekme_query()
+        listmodel = Gtk.ListStore(str, str, str,str,str)
+        for i in range(len(self.hasta_listesi)):
+            listmodel.append(self.hasta_listesi[i])
 
         self.view = Gtk.TreeView(model=listmodel)
-        for i, column in enumerate(columns):
+        for i, column in enumerate(hasta_columns):
             cell = Gtk.CellRendererText()
-
-            if i == 0:
-                cell.props.weight_set = True
-                cell.props.weight = Pango.Weight.BOLD
-
             col = Gtk.TreeViewColumn(column, cell, text=i)
             self.view.append_column(col)
+
+    def ilac_tablo(self):
+        self.ilac_vericekme_query()
+        listmodel = Gtk.ListStore(str, str, str ,str ,str, str)
+        for i in range(len(self.ilac_listesi)):
+            listmodel.append(self.ilac_listesi[i])
+
+        self.ilac_view = Gtk.TreeView(model=listmodel)
+        for i, column in enumerate(ilac_columns):
+            cell = Gtk.CellRendererText()
+            col = Gtk.TreeViewColumn(column, cell, text=i)
+            self.ilac_view.append_column(col)
+
+    def cart_tablo(self):
+        listmodel = Gtk.ListStore(str, str, str ,str ,str, str)
+        #for i in range(len(self.ilac_listesi)):
+        #    listmodel.append(self.ilac_listesi[i])
+
+        self.cart_view = Gtk.TreeView(model=listmodel)
+        for i, column in enumerate(ilac_columns):
+            cell = Gtk.CellRendererText()
+            col = Gtk.TreeViewColumn(column, cell, text=i)
+            self.cart_view.append_column(col)
 
 
     #### Veri Tabanı Fonksiyonları ####
@@ -186,17 +249,13 @@ class MyWindow(Gtk.Window):
     def baglanti_baslat(self):
         self.con = sqlite3.connect('pharmacy.db')
         self.cursor = self.con.cursor()
-        
-    def kullanici_tablo_olustur(self):
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS users (ID TEXT, Password TEXT)")
-        self.con.commit()
     
     def kullanici_ekle_query(self,ids,passw):
-        self.cursor.execute("INSERT INTO users Values(?,?)",(ids,passw))
+        self.cursor.execute("INSERT INTO users(USERNAME,PASSWORD) Values(?,?)",(ids,passw))
         self.con.commit()
     
     def kullanici_giris_query(self,ids,passw):
-        self.cursor.execute("SELECT * FROM users WHERE ID == ? AND PASSWORD == ?",(ids,passw))
+        self.cursor.execute("SELECT * FROM users WHERE USERNAME == ? AND PASSWORD == ?",(ids,passw))
         liste = self.cursor.fetchall()
 
         if len(liste) == 1:
@@ -207,9 +266,28 @@ class MyWindow(Gtk.Window):
             self.main_Table.attach(self.main_hataLabel,2,8,8,9)
             self.main_Table.show_all()
     
-    def hasta_tablo_olustur(self):
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS patients (Name TEXT, Surname TEXT, Mail TEXT)")
-        self.con.commit()
+    def hasta_vericekme_query(self):
+        self.cursor.execute("SELECT * FROM patients")
+        hasta_list = self.cursor.fetchall()
+        self.hasta_listesi = list()
+        for i inilac_view list(hasta_list):
+            gecici_liste = list()
+            for j in i:
+                gecici_liste.append(str(j))
+            
+            self.hasta_listesi.append(gecici_liste)
+    
+    def ilac_vericekme_query(self):
+        self.cursor.execute("SELECT * FROM medicines")
+        ilac_list = self.cursor.fetchall()
+        self.ilac_listesi = list()
+        for i in list(ilac_list):
+            gecici_liste = list()
+            for j in i:
+                gecici_liste.append(str(j))
+            
+            self.ilac_listesi.append(gecici_liste)
+
 
     #### Prio 2 Veri Tabanı Fonksiyonları ####
     
